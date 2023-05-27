d24b0d699c524648cbc727344a20c7dc9766b452
Xan <direct-ambles-0y@icloud.com>
2023-05-27 01:05:10 +0000

    :fire: break generic wordpress tweaks out into their own plugin

--- embold-sage10-tweaks.php
+++ embold-sage10-tweaks.php
@@ -4,7 +4,7 @@
  * Plugin Name:        emBold Sage10 Tweaks
  * Plugin URI:         https://embold.com
  * Description:        A collection of tweaks and changes to the Sage 10 framework.
- * Version:            0.6.0
+ * Version:            0.7.0
  * Author:             emBold
  * Author URI:         https://embold.com/
  * Primary Branch:     master
@@ -25,17 +25,6 @@ function embold_sage10_tweaks_init() {
 
     // Enqueue the frontend CSS to the editor
     $plugin->addCssToEditor();
-
-    // Allow SVG uploads
-    $plugin->addSvgSupport();
-
-    if (wp_get_environment_type() == 'development') {
-        // Defer scripts to try to avoid Coders 502 errors
-        $plugin->deferScripts();
-
-        // Async scripts to try to avoid Coders 502 errors
-        $plugin->asyncScripts();
-    }
 
     $plugin->addParagraphBlockClass();
 
